1df09c0318c33983f50a66849280b41eeb5bbb22
Julien Déray <julien@deray.fr>
2016-02-04 11:24:05 +0000

    sets up c3 pie chart

--- public/app/dashboard/dashboard.controller.js
+++ public/app/dashboard/dashboard.controller.js
@@ -44,6 +44,24 @@
             }
 
             vm.loansAcquiredPerDayOption = loansAcquiredService.barChartOptions;
+
+            const chart = c3.generate({
+                bindto: "#allocation-pie",
+                data: {
+                    columns: [
+                        ['data1', 30],
+                        ['data2', 120]
+                    ],
+                    type : 'pie',
+                    onclick: function (d, i) { console.log("onclick", d, i); },
+                    onmouseover: function (d, i) { console.log("onmouseover", d, i); },
+                    onmouseout: function (d, i) { console.log("onmouseout", d, i); }
+                },
+                size: {
+                    height: 200,
+                    width: 200
+                }
+            });
         }
     }
 
