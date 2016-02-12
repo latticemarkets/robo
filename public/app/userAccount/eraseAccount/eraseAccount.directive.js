dd0a931ff4e881300566b233b3098b825a6c3949
Julien Déray <julien@deray.fr>
2016-02-12 12:01:43 +0000

    implements destroy user within the directive

--- public/app/userAccount/eraseAccount/eraseAccount.directive.js
+++ public/app/userAccount/eraseAccount/eraseAccount.directive.js
@@ -5,16 +5,22 @@
         .module('app')
         .directive('eraseAccount', eraseAccount);
 
-    eraseAccount.$inject = ['userService', 'notificationService','authenticationService'];
+    eraseAccount.$inject = ['userService', 'notificationService','authenticationService', '$timeout', '$location'];
 
-      function eraseAccount(userService, notificationService, authenticationService) {
+      function eraseAccount(userService, notificationService, authenticationService, $timeout, $location) {
         return {
             replace: true,
             restrict: 'E',
-            scope: {
-                userPromise: '='
-            },
+            scope: {},
             templateUrl: '/assets/app/userAccount/eraseAccount/eraseAccount.html',
+            link(scope) {
+                scope.erase = () => {
+                    userService.destroyUser(authenticationService.getCurrentUsersEmail(), () => {
+                        notificationService.success("Your account have been successfully deleted. You will be redirected to the main page.");
+                        $timeout(() => $location.path('/'), 5000);
+                    });
+                };
+            }
           };
 
       }
