64dcd041c747231daed62fe3d47dfbfa2e5e9da3
Julien Déray <julien@deray.fr>
2016-02-13 12:59:36 +0000

    adds priority arrangement by drag&drop

--- public/app/services/notification.service.js
+++ public/app/services/notification.service.js
@@ -25,7 +25,7 @@
             this.toastr.error(message, 'Error');
         }
 
-        apiError() {
+        apiError(errorCallback) {
             return response => {
                 switch(response.status) {
                     case 400:
@@ -38,6 +38,9 @@
                     default:
                         this.toastr.error('The server returned an unknown error.', 'Server error');
                         break;
+                }
+                if (errorCallback) {
+                    errorCallback();
                 }
             };
         }
