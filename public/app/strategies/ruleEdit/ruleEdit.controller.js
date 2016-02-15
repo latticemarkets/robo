1e3bb9aac3afe6e6a03bf9b3e7a452a0c8e72fcb
Julien Déray <julien@deray.fr>
2016-02-15 11:02:31 +0000

    reorders the code

--- public/app/strategies/ruleEdit/ruleEdit.controller.js
+++ public/app/strategies/ruleEdit/ruleEdit.controller.js
@@ -18,41 +18,65 @@
         constructor(authenticationService, $routeParams, constantsService, userService, $location, cssInjector, CriteriaService) {
             var vm = this;
 
-            cssInjector.add("assets/stylesheets/homer_style.css");
-
             const defaultName = "New Rule";
-
             const email = authenticationService.getCurrentUsersEmail();
+
             const platform = $routeParams.platform;
-            if (!constantsService.platforms().some(realPlatform => realPlatform == platform)) {
-                $location.path('/strategies');
+            const ruleId = $routeParams.ruleId;
+
+            injectCss();
+            checkUrlParameters();
+            getCriteria();
+
+
+
+            /**
+             * Functions
+             */
+
+            function injectCss() {
+                cssInjector.add("assets/stylesheets/homer_style.css");
             }
 
-            const ruleId = $routeParams.ruleId;
-            if (ruleId) {
-                vm.spinner = true;
-                userService.userData(email, response => {
-                    response.data.platforms.some(p => {
-                        if (p.name == platform) {
-                            if (!p.rules.some(rule => {
-                                if (rule.id == ruleId) {
-                                    vm.rule = rule;
+            function getCriteria() {
+                CriteriaService.criteria(response => vm.criteria = response.data);
+            }
+
+            function checkUrlParameters() {
+                checkPlatform();
+                checkRuleId();
+
+                function checkPlatform() {
+                    if (!constantsService.platforms().some(realPlatform => realPlatform == platform)) {
+                        $location.path('/strategies');
+                    }
+                }
+
+                function checkRuleId() {
+                    if (ruleId) {
+                        vm.spinner = true;
+                        userService.userData(email, response => {
+                            response.data.platforms.some(p => {
+                                if (p.name == platform) {
+                                    if (!p.rules.some(rule => {
+                                            if (rule.id == ruleId) {
+                                                vm.rule = rule;
+                                                return true;
+                                            }
+                                        })) {
+                                        $location.path(`/strategies/rules/${platform}`);
+                                    }
                                     return true;
                                 }
-                            })) {
-                                $location.path(`/strategies/rules/${platform}`);
-                            }
-                            return true;
-                        }
-                    });
-                    vm.spinner = false;
-                });
+                            });
+                            vm.spinner = false;
+                        });
+                    }
+                    else {
+                        vm.rule = {name: defaultName, criteria: []};
+                    }
+                }
             }
-            else {
-                vm.rule = { name: defaultName, criteria: [] };
-            }
-
-            CriteriaService.criteria(response => vm.criteria = response.data);
         }
     }
 
