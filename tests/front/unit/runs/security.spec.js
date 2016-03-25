ae704dafe9ef0004e58ed7c2da08c6f1dcd39eb0
Julien Déray <julien@deray.fr>
2016-03-25 13:00:44 +0000

    reformats security.run tests

--- tests/front/unit/runs/security.spec.js
+++ tests/front/unit/runs/security.spec.js
@@ -7,11 +7,11 @@
  */
 
 /**
-* @author : bastienguerineau
-* Created on 17/03/2016
-*/
+ * @author : bastienguerineau
+ * Created on 17/03/2016
+ */
 
-describe('module: app', function() {
+describe('module: app', function () {
     let $rootScope,
         $location,
         $window,
@@ -19,41 +19,41 @@ describe('module: app', function() {
         $http,
         $injector;
 
-        beforeEach(() => {
-            module('app');
-            module($provide => {
-                $provide.service('$cookies', () => {
-                    return {
-                        putObject: jasmine.createSpy('putObject'),
-                        get: jasmine.createSpy('get'),
-                        remove: jasmine.createSpy('remove'),
-                        getObject: jasmine.createSpy('getObject')
-                    };
-                });
+    beforeEach(() => {
+        module('app');
+        module($provide => {
+            $provide.service('$cookies', () => {
+                return {
+                    putObject: jasmine.createSpy('putObject'),
+                    get: jasmine.createSpy('get'),
+                    remove: jasmine.createSpy('remove'),
+                    getObject: jasmine.createSpy('getObject')
+                };
             });
         });
+    });
 
-        beforeEach(inject(($injector) => {
-            $rootScope = $injector.get('$rootScope');
-            $http = $injector.get('$http');
-        }));
+    beforeEach(inject(($injector) => {
+        $rootScope = $injector.get('$rootScope');
+        $http = $injector.get('$http');
+    }));
 
-        beforeEach(inject(function (_$location_, _$window_, _$cookies_) {
-            $cookies = _$cookies_;
-            $window = _$window_;
-            $location = _$location_;
-        }));
+    beforeEach(inject(function (_$location_, _$window_, _$cookies_) {
+        $cookies = _$cookies_;
+        $window = _$window_;
+        $location = _$location_;
+    }));
 
     describe('run security', () => {
-    let token, email;
+        let token, email;
         beforeEach(() => {
             token = 'token';
             email = 'email';
-            $rootScope.globals = { currentUser : { email: email, token: token } };
+            $rootScope.globals = {currentUser: {email: email, token: token}};
         });
 
         it('should store the email and token into the root scope', () => {
-            expect($rootScope.globals.currentUser).toEqual({ email: email, token: token });
+            expect($rootScope.globals.currentUser).toEqual({email: email, token: token});
         });
 
         it('should sets the token as default http header', () => {
@@ -65,44 +65,44 @@ describe('module: app', function() {
         });
 
         it('should store the user\'s credentials in a cookie', () => {
-            expect(_$cookies.putObject).toHaveBeenCalledWith('globals', { currentUser: { email: email, token: token } });
+            expect(_$cookies.putObject).toHaveBeenCalledWith('globals', {currentUser: {email: email, token: token}});
         });
-  });
-
-  describe('run location change start', () => {
-    let token, email;
-    beforeEach(() => {
-      token = undefined;
-      email = undefined;
-      $rootScope.$broadcast('$locationChangeStart');
-      $rootScope.globals = { currentUser : { email: email, token: token } };
-      // $location.path() = '/signin';
-      // $cookies.get() = 'connected';
     });
 
-    it('should remove the "connected" cookie', () => {
-        expect(_$cookies.remove).toHaveBeenCalledWith('connected');
+    describe('run location change start', () => {
+        let token, email;
+        beforeEach(() => {
+            token = undefined;
+            email = undefined;
+            $rootScope.$broadcast('$locationChangeStart');
+            $rootScope.globals = {currentUser: {email: email, token: token}};
+            // $location.path() = '/signin';
+            // $cookies.get() = 'connected';
+        });
+
+        it('should remove the "connected" cookie', () => {
+            expect(_$cookies.remove).toHaveBeenCalledWith('connected');
+        });
+
+        it('should change locacation href', () => {
+            expect($window.location.href).toHaveBeenCalledWith('/?flag=expired');
+        });
     });
 
-    it('should change locacation href', () => {
-        expect($window.location.href).toHaveBeenCalledWith('/?flag=expired');
-    });
-  });
+    describe('run location change start', () => {
+        let token, email;
+        beforeEach(() => {
+            token = undefined;
+            email = undefined;
+            $rootScope.$broadcast('$locationChangeStart');
+            $rootScope.globals = {currentUser: {email: email, token: token}};
+            // $location.path() = '/signin';
+            // $cookies.get() = 'notconnected';
+        });
 
-  describe('run location change start', () => {
-    let token, email;
-    beforeEach(() => {
-      token = undefined;
-      email = undefined;
-      $rootScope.$broadcast('$locationChangeStart');
-      $rootScope.globals = { currentUser : { email: email, token: token } };
-      // $location.path() = '/signin';
-      // $cookies.get() = 'notconnected';
+        it('should change locacation href', () => {
+            expect($window.location.href).toHaveBeenCalledWith('/?flag=unauthorized');
+        });
     });
-
-    it('should change locacation href', () => {
-        expect($window.location.href).toHaveBeenCalledWith('/?flag=unauthorized');
-    });
-  });
 
 });
